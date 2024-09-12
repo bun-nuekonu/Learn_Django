f741a9958d8261aaf04f708d007daa2e736c78dc
bun <bun29268@gmail.com>
2024-09-13 00:19:16 +0900

    フォームの勉強

--- hello/views.py
+++ hello/views.py
@@ -1,17 +1,24 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 # Create your views here.
 
-def index(request):
-  params = {
-    'title':'Hello',
-    'message':'your data:',
-    'form': HelloForm()
-  }
-  if (request.method == 'POST'):
-    params['message'] = '名前:' + request.POST['name'] + \
-      '<br>メール' + request.POST['mail'] + \
-      '<br>年齢' + request.POST['age']
-    params['form'] = HelloForm(request.POST)
-  return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+
+  def __init__(self):
+    self.params = {
+      'title': 'Hello',
+      'message': 'your data:',
+      'form': HelloForm()
+    }
+  
+  def get(self, request):
+    return render(request, 'hello/index.html', self.params)
+  
+  def post(self, request):
+    msg = 'おまえは<b>' + request.POST['name'] + '()' + request.POST['age'] + '()' + 'さんです。<br>メールアドレスは<b>', + request.POST['mail']
+    self.params['message'] = msg
+    self.params['form'] = HelloForm(request.GET)
+    return render(request, 'hello/index', self.params)
